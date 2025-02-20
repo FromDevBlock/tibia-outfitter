70c8beac1c7e38c15e99442b0bfe511dfed4f852
FromDevBlock? <137526449+FromDevBlock@users.noreply.github.com>
2025-02-20 03:09:46 -0300

    Update outfit.php

--- outfit.php
+++ outfit.php
@@ -6,15 +6,15 @@ $outfit = new Outfit(array(
     'query' => true,
     'hexmount' => true,
     'queries' => array(
-        'looktype' => 'a',
-        'addons' => 'b',
-        'head' => 'c',
-        'body' => 'd',
-        'legs' => 'e',
-        'feet' => 'f',
-        'mount' => 'g',
-        'direction' => 'h',
-        'movement' => 'i'
+        'looktype' => 'id',
+        'addons' => 'addons',
+        'head' => 'head',
+        'body' => 'body',
+        'legs' => 'legs',
+        'feet' => 'feet',
+        'mount' => 'mount',
+        'direction' => 'direction',
+        'movement' => 'movement'
     )
 ));
 
